1bfac2ffe950bba14ad35fc7d36f09b01c35b2f2
Rayan <rayan13170@protonmail.com>
2024-01-04 17:10:17 +0100

    New branch

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -20,8 +20,6 @@ const success = response.data.value?.success;
 // Before filtering
 let articles: Article[] | undefined;
 
-const filteredCategories = ref<string[]>([]);
-
 if (success) {
     // Check if response data is an array of articles
     if (Array.isArray(response.data.value?.data)) {
@@ -33,6 +31,30 @@ if (success) {
 } else {
     state.value.error = true;
 }
+
+
+const publish = (articleId: number) => {
+    $fetch(`/api/v1/articles/update/${articleId}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            published: true,
+        }),
+    });
+};
+
+const unpublish = (articleId: number) => {
+    $fetch(`/api/v1/articles/update/${articleId}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            published: false,
+        }),
+    });
+};
+
+const update = (articleId: number) => {
+    console.log('update')
+};
+
 </script>
 
 
@@ -43,7 +65,7 @@ if (success) {
             <h1>Dashboard</h1>
             <p>Here you can manage your articles.</p>
         </div>
-        <table class="table table-striped table-dark table-hover w-75 mx-auto">
+        <table class="table table-striped table-dark table-hover table-responsive mx-auto text-center">
             <thead>
                 <tr>
                     <th scope="col">#</th>
@@ -51,21 +73,30 @@ if (success) {
                     <th scope="col">Date</th>
                     <th scope="col">Category</th>
                     <th scope="col">Tags</th>
-                    <th scope="col">Delete</th>
-                    <th scope="col">Unpublish</th>
+                    <th scope="col">Published</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="article in articles">
+                <tr v-for="article in articles" :key="article.id">
                     <th scope="row">{{ article.id }}</th>
-                    <td>{{ article.title }}</td>
+                    <td @click="update(article.id)">{{ article.title }}</td>
                     <td>{{ article.created_at }}</td>
                     <td>{{ article.category.name }}</td>
                     <td>{{ article.tags.name }}</td>
-                    <td>{{ article.published }}</td>
-                    <td>{{ article.tags.name }}</td>
+                    <td v-if="article.published" @click="unpublish(article.id)"><i class="bi bi-check-lg"></i></td>
+                    <td v-else @click="publish(article.id)"><i class="bi bi-x-lg"></i></td>
                 </tr>
             </tbody>
         </table>
     </div>
 </template>
+
+
+
+<style scoped>
+i {
+    font-size: 1.2rem;
+    color: var(--text);
+    background-color: transparent;
+}
+</style>
