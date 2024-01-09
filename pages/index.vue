ed2116675a2d3b1f25226afa589f4b39ef8e1879
Rayan <rayan13170@protonmail.com>
2024-01-09 19:12:56 +0100

    added progress bar

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,18 @@
         <div class="container py-5">
             <NuxtLink to="/" class="navbar-brand title">Rayan.sh</NuxtLink>
             <h1 class="text-center">Coming Soon ...</h1>
+            <div class="progress">
+                <div class="bar"></div>
+            </div>
         </div>
     </div>
 </template>
 
 
 <style scoped>
+body {
+    background: black;
+}
 
 h1 {
     background-color: transparent;
@@ -19,6 +25,7 @@ h1 {
     font-size: 70px;
     font-weight: 500;
 }
+
 .title {
     font-size: 3rem;
     font-weight: 600;
@@ -43,12 +50,62 @@ h1 {
 }
 
 
+
+/* Progress bar  */
+
+.progress {
+    --progress: 60%;
+
+    width: 30%;
+    height: 40px;
+    margin: 4em auto;
+    border: 1px solid #fff;
+    padding: 8px 10px;
+    box-shadow: 0 0 10px #aaa;
+}
+
+.progress .bar {
+    width: var(--progress);
+    height: 100%;
+    background: linear-gradient(#86bbe4, #6096c3, #86bbe4);
+
+    background-repeat: repeat;
+    box-shadow: 0 0 10px 0px rgb(0, 0, 0);
+    animation:
+        shine 4s ease-in infinite,
+        end 1s ease-out 1 7s;
+    transition: width 3s ease 3s;
+}
+
+@property --progress {
+    syntax: "<length>";
+    initial-value: 0%;
+    inherits: true;
+}
+
+@keyframes shine {
+    0% {
+        background-position: 0 0;
+    }
+
+    100% {
+        background-position: 0 50px;
+    }
+}
+
+
+
 @media (max-width: 768px) {
     h1 {
         background-color: transparent;
-        padding-top: 80px;
+        padding-top: 50px;
         font-size: 50px;
         font-weight: 500;
     }
+    .progress {
+        width: 90%;
+        height: 30px;
+    }
 }
+
 </style>
