1b5789e1aa16d0410718e5dece3cd42d0f0f7347
rayan <rayan13170@protonmail.com>
2024-02-01 23:44:24 +0100

    enhanced navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -23,16 +23,15 @@ const showMenu = ref(false);
 <template>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <div class="w-100 text-end mx-3 sm-content">
+            <div class="w-100 mx-3 sm-content">
                 <div class="navbar-nav">
-                    <a class="nav-link main" aria-current="page" href="#">rayan.sh</a>
-                    <a class="nav-link" href="#">_hello</a>
-                    <a class="nav-link px-4" href="#">_about-me</a>
-                    <a class="nav-link">_projects</a>
-                    <a class="nav-link px-4">_blog</a>
-                    <a class="nav-link contact" href="#">_contact-me</a>
+                    <NuxtLink class="nav-link me-auto" aria-current="page" to="/">rayan.sh</NuxtLink>
+                    <NuxtLink class="nav-link" to="/">_hello</NuxtLink>
+                    <NuxtLink class="nav-link px-4" to="/about">_about-me</NuxtLink>
+                    <NuxtLink class="nav-link">_projects</NuxtLink>
+                    <NuxtLink class="nav-link px-4" to="/blog">_blog</NuxtLink>
+                    <a class="nav-link contact" href="mailto:rayan13170@protonmail.com">_contact-me</a>
                 </div>
-
             </div>
         </div>
     </nav>
@@ -40,9 +39,8 @@ const showMenu = ref(false);
 
 
 <style scoped>
-
 .navbar {
-    height: 49px;
+    height: 60px;
     border-bottom: 1px solid #1E2D3D;
 }
 
@@ -50,13 +48,19 @@ const showMenu = ref(false);
     color: var(--text);
 }
 
-.main {
-    margin-right: 45px;
-}
-
 .contact {
     margin-left: auto;
 }
+
+/* HOVERS */
+
+.nav-link:hover, .contact:hover {
+  background-color: #1e2d3d74;
+  color: white;
+}
+
+
+/* END HOVERS */
 
 /* TOGGLE BUTTON */
 
