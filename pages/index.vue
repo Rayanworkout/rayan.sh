ebc321a43373e6df20828339804cd2d1e422e75b
rayan <rayan13170@protonmail.com>
2024-02-02 00:51:34 +0100

    started to work on index

--- pages/index.vue
+++ pages/index.vue
@@ -39,18 +39,70 @@ onMounted(() => {
     <div class="bg-container">
         <Navbar />
         <div class="container py-5">
-
+            <!-- gradients -->
+            <div class="css-blurry-gradient-blue"></div>
+            <div class="css-blurry-gradient-green"></div>
+            <div class="hero">
+                <div class="row">
+                    <div class="col-md-6 head">
+                        <span>
+                            Hi all, I am
+                        </span>
+                        <h1>Rayan</h1>
+                        <h2>> Backend Developer</h2>
+                    </div>
+                    <div class="col-md-6">Hello 2</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 
 <style scoped>
-
 .bg-container {
     background-color: var(--new-background);
     height: 100vh;
 }
 
+h1 {
+    font-size: 58px;
+    line-height: 1;
+    color: #E5E9F0;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+}
 
+.head span {
+    font-size: 18px;
+    line-height: 1;
+    color: #E5E9F0;
+}
+
+.css-blurry-gradient-blue {
+    position: fixed;
+    bottom: 25%;
+    right: 5%;
+    width: 300px;
+    height: 300px;
+    border-radius: 0% 0% 50% 50%;
+    rotate: 10deg;
+    filter: blur(70px);
+    background: radial-gradient(circle at 50% 50%, rgba(77, 91, 206, 1), rgba(76, 0, 255, 0));
+    opacity: 0.5;
+    z-index: 10;
+}
+
+.css-blurry-gradient-green {
+    position: absolute;
+    top: 20%;
+    right: 30%;
+    width: 300px;
+    height: 300px;
+    border-radius: 0% 50% 0% 50%;
+    filter: blur(70px);
+    background: radial-gradient(circle at 50% 50%, rgba(67, 217, 173, 1), rgba(76, 0, 255, 0));
+    opacity: 0.5;
+    z-index: 10;
+}
 </style>
