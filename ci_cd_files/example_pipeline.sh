e481f205623c21ba2415182a035243a5885f3479
rayan <rayan13170@protonmail.com>
2024-02-14 21:44:01 +0100

    deleted tags

--- ci_cd_files/example_pipeline.sh
+++ ci_cd_files/example_pipeline.sh
@@ -11,6 +11,11 @@
 # Stop if an error occurs
 set -e
 
+# Telegram bot token and chat id
+telegram_bot_token="BOT_TOKEN"
+telegram_chat_id="CHAT_ID"
+
+
 echo "> Pulling changes ..."
 git pull origin main
 echo "> Done"
@@ -32,3 +37,8 @@ echo "> Restarting Service ..."
 sudo systemctl restart rayan.sh.service
 
 echo "> Done, check website at https://rayan.sh"
+
+# Send a telegram alert to notify myself that the build was successful
+msg=$(jq -rn --arg x "Last build of rayan.sh was successful" '$x|@uri')
+tg_url="https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&text=$msg"
+resp=$(curl -s "$tg_url")
