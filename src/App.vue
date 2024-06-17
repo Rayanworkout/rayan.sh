3898ea8e016fe8fbb4e30bd0fd24857cd6e5e4db
rayan <rayan13170@protonmail.com>
2024-06-17 02:27:57 +0200

    cleaned some more

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,92 @@
 <script setup>
-import Navbar from './navbar.component.vue'
+
+  import MacTerminal from './components/terminal.component.vue';
+
 </script>
 
+
 <template>
-  <div>
-    <!-- <Navbar /> -->
-    <router-view></router-view>
+  <div class="container py-5">
+    <div class="my-5">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="head">
+            <div class="hello"><span style="font-size: 18px;">
+                Hi all, I am
+              </span>
+              <h1>Rayan<span class="cursor">_</span></h1>
+              <h2>> Backend Developer</h2>
+              <h2>> Devops Enthusiast</h2>
+            </div>
+            <div>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <MacTerminal />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+
+<style scoped>
+  .head {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+
+  h1 {
+    font-size: 45px;
+    line-height: 1;
+    color: #E5E9F0;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+
+  .head span,
+  .head a {
+    font-size: 17px;
+    line-height: 1;
+  }
+
+  .head h2 {
+    font-size: 30px;
+    line-height: 1;
+    color: white;
+    font-family: 'JetBrains Mono', 'monospace';
+  }
+
+  p,
+  .hello span {
+    color: #c8c8c8b9;
+    font-family: 'JetBrains Mono', 'monospace';
+  }
+
+  /* MEDIA QUERIES */
+
+  @media (max-width: 768px) {
+
+    .head h2 {
+      font-size: 23px;
+    }
+
+    h1 {
+      font-size: 35px;
+    }
+
+    .github span,
+    .github a,
+    .blog span,
+    .blog a {
+      font-size: 18px;
+    }
+  }
+
+  /* END MEDIA QUERIES */
+</style>
--- src/main.js
+++ src/main.js
@@ -10,23 +10,5 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css';
 
-// MAIN PAGES
-import indexPage from './index.vue'
-import projectsPage from './projects/projects.page.vue'
 
-// ROUTER
-import { createWebHistory, createRouter } from 'vue-router'
-
-const routes = [
-    { path: '/', component: indexPage },
-    { path: '/projects', component: projectsPage },
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
-
-createApp(App)
-    .use(router)
-    .mount('#app')
+createApp(App).mount('#app')
