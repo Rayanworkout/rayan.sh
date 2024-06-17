b7bf2f105fa12d757698e0f43f01dc9985e22a82
rayan <rayan13170@protonmail.com>
2024-06-17 02:22:07 +0200

    cleaned files

--- src/index.vue
+++ src/index.vue
@@ -1,25 +1,28 @@
 <script setup>
 
-    import DesktopIndex from './components/desktopIndex.component.vue';
-    import MobileIndex from './components/mobileIndex.component.vue';
-
+    import MacTerminal from './components/terminal.component.vue';
     document.title = 'Home';
-
- 
 
 </script>
 
+
 <template>
-    <div class="bg-container">
-        <div class="container py-5">
-            <!-- gradient -->
-            <div class="css-blurry-gradient-blue"></div>
-            <div class="hero my-5">
-                <div class="desktop">
-                    <DesktopIndex />
+    <div class="container py-5">
+        <div class="my-5">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="head">
+                        <div class="hello"><span style="font-size: 18px;">
+                                Hi all, I am
+                            </span>
+                            <h1>Rayan<span class="cursor">_</span></h1>
+                            <h2>> Backend Developer</h2>
+                            <h2>> Devops Enthusiast</h2>
+                        </div>
+                    </div>
                 </div>
-                <div class="mobile">
-                    <MobileIndex />
+                <div class="col-md-8">
+                    <MacTerminal />
                 </div>
             </div>
         </div>
@@ -28,29 +31,60 @@
 
 
 <style scoped>
-    .bg-container {
-        background-color: var(--background);
-        min-height: 110vh;
+    .head {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
+
+
+    h1 {
+        font-size: 45px;
+        line-height: 1;
+        color: #E5E9F0;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+    }
+
+    .head span,
+    .head a {
+        font-size: 17px;
+        line-height: 1;
+    }
+
+    .head h2 {
+        font-size: 30px;
+        line-height: 1;
+        color: white;
+        font-family: 'JetBrains Mono', 'monospace';
+    }
+
+    p,
+    .hello span {
+        color: #c8c8c8b9;
+        font-family: 'JetBrains Mono', 'monospace';
+    }
+
+    /* MEDIA QUERIES */
 
     @media (max-width: 768px) {
-        .desktop {
-            display: none;
+
+        .head h2 {
+            font-size: 23px;
         }
 
-        .mobile {
-            display: block;
-        }
-    }
-
-
-    @media (min-width: 768px) {
-        .desktop {
-            display: block;
+        h1 {
+            font-size: 35px;
         }
 
-        .mobile {
-            display: none;
+        .github span,
+        .github a,
+        .blog span,
+        .blog a {
+            font-size: 18px;
         }
     }
+
+    /* END MEDIA QUERIES */
 </style>
