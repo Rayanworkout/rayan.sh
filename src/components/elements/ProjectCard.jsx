48e4e45221599196a9878946c6037fa7c8297b41
rayan <rayan13170@protonmail.com>
2024-06-30 19:35:28 +0200

    added projects in resume

--- src/components/elements/ProjectCard.jsx
+++ src/components/elements/ProjectCard.jsx
@@ -2,15 +2,15 @@
 export default function ProjectCard({ project }) {
     return (
         <div className="rounded shadow-lg">
-            <img className="w-full" src={project.image} alt={project.title} />
+            <img className="w-full" src={`/images/projects/${project.image}`} alt={project.name} />
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{project.title}</div>
+                <div className="font-bold text-xl mb-2">{project.name}</div>
                 <p className="text-gray-700 text-base">
                     {project.description}
                 </p>
             </div>
             <div className="px-6 pt-4 pb-2">
-                <a href={project.link} className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <a href={project.link} target="_blank" className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     View Project
                 </a>
             </div>
