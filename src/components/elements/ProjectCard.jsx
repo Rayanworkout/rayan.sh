a1564abb2fee1ec024624fc3462871b9613338ba
rayan <rayan13170@protonmail.com>
2024-06-30 22:38:19 +0200

    fixed carousel

--- src/components/elements/ProjectCard.jsx
+++ src/components/elements/ProjectCard.jsx
@@ -1,18 +1,32 @@
 
 export default function ProjectCard({ project }) {
+
+
     return (
         <div className="rounded shadow-lg">
-            <img className="w-full" src={`/images/projects/${project.image}`} alt={project.name} />
+            <img className="w-full" src={`/images/projects/${project.image}.jpg`} alt={project.name} />
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">{project.name}</div>
                 <p className="text-gray-700 text-base">
                     {project.description}
                 </p>
+                <p>
+                    {project.keywords.map((keyword, idx) => (
+                        <span key={idx} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mt-2">
+                            {keyword}
+                        </span>
+                    ))}
+                </p>
             </div>
             <div className="px-6 pt-4 pb-2">
                 <a href={project.link} target="_blank" className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    View Project
+                    Direct Link
                 </a>
+                <a href={project.githubLink} target="_blank" className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-3 rounded">
+                    Source Code & Docs
+                </a>
+
+
             </div>
         </div>
     );
--- src/components/structure/Projects.jsx
+++ src/components/structure/Projects.jsx
@@ -27,15 +27,15 @@ const Carousel = ({ children, autoSlide = true, autoSlideInterval = 5000 }) => {
 
 
     return (
-        <div className='overflow-hidden relative'>
-            <div className='flex' style={{ width: '100%', transform: `translateX(-${current * 100}%)` }}>
+        <div className='overflow-hidden relative rounded border'>
+            <div className='flex transition-transform ease-out duration-1000' style={{ width: '100%', transform: `translateX(-${current * 100}%)` }}>
                 {children.map((slide, index) => (
                     <div key={index} className="flex-none w-full">
                         {slide}
                     </div>
                 ))}
             </div>
-            <div className="absolute inset-0 flex items-center justify-between p-4">
+            <div className="absolute inset-0 max-h-[60%] bottom-[100px] flex items-end justify-between p-4">
                 <button onClick={prevSlide} className="p-1 rounded-full shadow bg-white-80 text-gray-800 hover:bg-white">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
@@ -46,15 +46,6 @@ const Carousel = ({ children, autoSlide = true, autoSlideInterval = 5000 }) => {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
                     </svg>
                 </button>
-            </div>
-            <div className="absolute bottom-4 right-0 left-0">
-                <div className="flex items-center justify-center gap-2">
-                    {
-                        children.map((_, idx) => (
-                            <div key={idx} className={`transition-all w-3 h-3 bg-white rounded-full ${current === idx ? "p-2" : "bg-opacity-50"}`}></div>
-                        ))
-                    }
-                </div>
             </div>
         </div>
     );
@@ -67,7 +58,7 @@ export default function Projects() {
     return (
         <div className="projects mx-auto max-w-lg">
             <h1 className="text-center text-4xl font-bold mt-20 mb-10">Projects</h1>
-            <Carousel autoSlide={true}>
+            <Carousel autoSlide={false}>
                 {Resume.projects.map((project, idx) => (
                     <ProjectCard key={idx} project={project} />
                 ))}
